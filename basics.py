67db080d37a39dc22d8b8928a6294bcc15345787
Sabuat Urbina Ribeiro <90584688+sabuat@users.noreply.github.com>
2022-10-23 22:29:19 -0300

    Add files via upload

---
+++ basics.py
@@ -0,0 +1,65 @@
+# #EXERCICIO 1
+# valor_1 = int(input("Ingrese el valor: "))
+# valor_2 = valor_1 - (valor_1 * 0.15)
+# print("Su nuevo valor es", format(valor_2))
+
+# #EXERCICIO 2
+# edad = int(input("ingrese su edad "))
+# if edad == 0 or edad > 150:
+#     print("Valor equivocado")
+# salario = float(input("ingrese su salario "))
+# if salario == 0:
+#     print("Valor equivocado")
+# genero = input("ingrese su genero (Fem, Mas, Out) ")
+# gen = genero.lower()
+# if gen == 'fem' or gen == 'mas':
+#     print("gracias")
+# elif gen == 'out':
+#     print("gracias")
+# else:
+#     print("ingrese un valor correcto")
+
+# #EXERCICIO 3
+# contador = 0
+# nome = input("Ingrese el nombre del sospechoso ")
+
+# a = input("Mora perto da vítima? ")
+# a = a.lower()
+# if a == 'sim':
+#     contador = contador + 1
+
+# b = input("Já trabalhou com a vítima? ")
+# b = b.lower()
+# if b == 'sim':
+#     contador = contador + 1
+
+# c = input("Telefonou para a vítima? ")
+# c = c.lower()
+# if c == 'sim':
+#     contador = contador + 1
+
+# d = input("Esteve no local do crime? ")
+# d = d.lower()
+# if d == 'sim':
+#     contador = contador + 1
+
+# e = input("Devia para a vítima? ")
+# e = e.lower()
+# if e == 'sim':
+#     contador = contador + 1
+
+# if contador == 5:
+#     print ("{} es el posible asesino" .format(nome))
+# elif contador == 4 or contador ==3:
+#     print ("{} es sospechoso del asesinato" .format(nome))
+# elif contador < 3:
+#     print ("{} no es sospechoso del asesinato" .format(nome))
+
+# #EXERCICIO 4
+# contador = 1
+# tabuada = int(input("Tabla del "))
+# while contador < 11:
+#     resultado = tabuada * contador
+#     print (resultado)
+#     contador = contador + 1
+
---
+++ basics2.py
@@ -0,0 +1,41 @@
+# #EXERCICIO 1
+# lista = [0,1,2,3,4,5,6,7,8,9]
+# for numero in lista:
+#     if numero % 2 == 0 and numero != 0:
+#         print (numero, "es par")
+
+# #EXERCICIO 2
+# palabra = input("ingrese una palabra ")
+# for letra in palabra:
+#     print(letra)
+
+# #EXERCICIO 3
+# lista_1 = [3,5,7]
+# lista_2 = [2,4,6]
+# lista_3 = []
+# x = 0
+# for valor in lista_1:
+#     novo = valor + lista_2[x]
+#     lista_3.append(novo)
+#     x = x + 1
+# print(lista_3)
+
+# #EXERCICIO 4
+meses_do_ano = {
+    "Janeiro":"31",
+    "fevereiro":"28",
+    "março":"31",
+    "Abril":"30",
+    "Maio":"31",
+    "Junho":"30",
+    "Julho":"31",
+    "Agosto":"31",
+    "Setembro":"30",
+    "Outubro":"31",
+    "Novembro":"30",
+    "Dezembro":"31"
+}
+print(meses_do_ano)
+
+# # EXERCICIO 5
+
